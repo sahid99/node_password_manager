e86f5ac6cd2099acee4d3019dd52990126ac277e
Sahid De la Rosa <sahid.enrico@gmail.com>
2021-09-03 18:43:18 -0500

    fix signIn password validation

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -10,6 +10,28 @@ const signIn = async (req, res) => {
 			username,
 			password
 		}
+		const result = await db.collection("password_manager").findOne({username});
+		
+		if(result.password == password){
+			return res.status(200).json({success: true});
+		}
+
+		return res.status(401).json({success: false});
+	}
+
+	return res.status(500).json({ message: "Server error"});
+}
+
+const signUp = async (req, res) => {
+	const { db } = await connectToDatabase();
+
+	const { username, password } = req.body;
+
+	if(username && password){
+		const user = {
+			username,
+			password
+		}
 		const result = await db.collection("password_manager").insertOne(user);
 		return res.status(200).json(result);
 	}
@@ -17,4 +39,4 @@ const signIn = async (req, res) => {
 	return res.status(500).json({ message: "Server error"});
 }
 
-module.exports = { signIn };
+module.exports = { signIn, signUp };
