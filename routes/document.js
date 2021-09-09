5ac46d2c1d29dbe57aa8eb7819f6344d3743b7ac
sahid <sahid.enrico@gmail.com>
2021-09-09 01:46:46 -0500

    auth to documents routes

--- routes/document.js
+++ routes/document.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const router = Router();
-
+const { verifyToken } = require("../controller/verifyToken");
 const { addDocument, getDocuments, modifyDocument } = require("../controller/document.controller");
 
-router.post("/getDocuments", getDocuments);
-router.post("/addDocument", addDocument);
-router.post("/modifyDocument", modifyDocument);
+router.post("/getDocuments", verifyToken, getDocuments);
+router.post("/addDocument", verifyToken, addDocument);
+router.post("/modifyDocument", verifyToken, modifyDocument);
 // router.post("/deleteDocument", {});
 
 module.exports = router;
