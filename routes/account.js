dd3e742ed26c63995781500cb870a97b042ef1cb
Sahid De la Rosa <sahid.enrico@gmail.com>
2021-09-10 19:04:46 -0500

    add account

--- routes/account.js
+++ routes/account.js
@@ -3,8 +3,8 @@ const router = Router();
 const { verifyToken } = require("../controller/verifyToken");
 const { addAccount, getAccounts, modifyAccount, deleteAccount } = require("../controller/account.controller");
 
-router.post("/getAccounts", verifyToken, addAccount);
-router.post("/addAccount", verifyToken, getAccounts);
+router.post("/getAccounts", verifyToken, getAccounts);
+router.post("/addAccount", verifyToken, addAccount);
 router.post("/modifyAccount", verifyToken, modifyAccount);
 router.post("/deleteAccount", verifyToken, deleteAccount);
 
